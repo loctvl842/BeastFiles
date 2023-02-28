463429ce1825ffebc1b67a0b4ddcdeb1544158e4
loctvl842 <loclepnvx@gmail.com>
2023-02-28 18:43:46 +0700

    chore: set default editor, terminal, browser
      - export EDITOR="nvim"
      - export TERMINAL="kitty"
      - export BROWSER="brave"

--- config/zsh/.config/zsh/exports.zsh
+++ config/zsh/.config/zsh/exports.zsh
@@ -2,6 +2,9 @@
 # HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
+export EDITOR="nvim"
+export TERMINAL="kitty"
+export BROWSER="brave"
 export PATH="$HOME/.local/bin":$PATH
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
