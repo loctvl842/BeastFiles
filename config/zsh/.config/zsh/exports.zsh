3c8126e27c4926ec1538bad424a5189cdce7b01c
loctvl842 <loclepnvx@gmail.com>
2024-04-06 13:26:08 +0700

    :beers: Chore: update

--- config/zsh/.config/zsh/exports.zsh
+++ config/zsh/.config/zsh/exports.zsh
@@ -11,6 +11,7 @@ export MANWIDTH=999
 export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$HOME/.local/share/go/bin:$PATH
 export PATH=$HOME/.fnm:$PATH
+export PATH=$PATH:$HOME/.dotnet/tools
 
 export GOPATH=$HOME/.local/share/go
 
@@ -103,3 +104,22 @@ export PATH=$ANDROID_HOME/platform-tools:$PATH
 export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export CXX=clang++
+
+export GVM_ROOT=$HOME/.gvm
+# Lazy load gvm
+gvm_lazy_load() {
+  echo 'gvm is loading...'
+  if [ -f "$GVM_ROOT/scripts/gvm" ]; then
+    # Source gvm script if it exists
+    source "$GVM_ROOT/scripts/gvm"
+  else
+    echo "gvm script not found. Make sure gvm is installed and configured correctly."
+  fi
+}
+
+# Override the gvm command to trigger lazy loading
+gvm() {
+  gvm_lazy_load
+  # Forward the gvm command to the actual gvm executable
+  command gvm "$@"
+}
