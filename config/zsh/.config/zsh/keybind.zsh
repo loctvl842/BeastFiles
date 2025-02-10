4b3bc5d88808ab7b9672c55d24432ebd76111cce
loctvl842 <loclepnvx@gmail.com>
2025-02-10 09:44:46 +0700

    chore: update

--- config/zsh/.config/zsh/keybind.zsh
+++ config/zsh/.config/zsh/keybind.zsh
@@ -6,4 +6,5 @@ bindkey "^p" up-line-or-beginning-search # Up
 bindkey "^n" down-line-or-beginning-search # Down
 bindkey -s '^V' 'nvim^M'
 bindkey -s '^o' 'ranger^M'
+bindkey -s '^[n' 'neofetch\n'
 bindkey -M menuselect '^[[Z' reverse-menu-complete
---
+++ wiki/troubleshooting.md
@@ -0,0 +1,7 @@
+# 1. `LarkSuite` cannot recognize fonts
+
+- How to solve:
+
+```bash
+sudo pacman -S ttf-liberation ttf-dejavu noto-fonts
+```
