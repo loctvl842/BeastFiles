4ef6722996b5313f4ce2a8a629d17e460826f5d7
loctvl842 <loclepnvx@gmail.com>
2025-02-17 13:40:59 +0700

    fix: do not include nameserver of company

--- bin/.local/bin/dns_switcher.sh
+++ bin/.local/bin/dns_switcher.sh
@@ -9,14 +9,34 @@ if [ -z "$wifi_name" ]; then
   exit 1
 fi
 
-if [ "$1" == "cloudflare" ]; then
-  sudo nmcli con mod "$wifi_name" ipv4.dns "1.1.1.1 1.0.0.1"
+# Function to switch DNS
+switch_dns() {
+  sudo nmcli con mod "$wifi_name" ipv4.ignore-auto-dns yes
+  sudo nmcli con mod "$wifi_name" ipv4.dns "$1"
   sudo nmcli con up "$wifi_name"
-  echo "Switched to Cloudflare DNS"
-elif [ "$1" == "default" ]; then
-  sudo nmcli con mod "$wifi_name" ipv4.dns ""
-  sudo nmcli con up "$wifi_name"
-  echo "Switched back to default DNS"
-else
-  echo "Usage: dns-switcher {cloudflare|default}"
-fi
+  echo "Switched to $2 DNS"
+}
+
+# Handle user input
+case "$1" in
+  cloudflare)
+    switch_dns "1.1.1.1 1.0.0.1" "Cloudflare"
+    ;;
+  google)
+    switch_dns "8.8.8.8 8.8.4.4" "Google"
+    ;;
+  default)
+    sudo nmcli con mod "$wifi_name" ipv4.ignore-auto-dns no
+    sudo nmcli con mod "$wifi_name" ipv4.dns ""
+    sudo nmcli con up "$wifi_name"
+    echo "Switched back to default DNS"
+    ;;
+  *)
+    echo "Usage: dns-switcher {cloudflare|google|default}"
+    exit 1
+    ;;
+esac
+
+# Display current DNS settings
+echo "Current DNS servers:"
+nmcli dev show | grep DNS
